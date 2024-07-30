29693279c929fc2a9d037fd03cd20b774faa3bba
deseyo <156849439+deseyo@users.noreply.github.com>
2024-07-30 15:48:37 +0200

    Refactor code to use class instead of plain constructors

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
-const addBtn = document.querySelector(".add-btn");
+const inputs = document.querySelectorAll('input');
 const authorInput = document.querySelector("#author");
 const titleInput = document.querySelector("#title");
 const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
+const addBtn = document.querySelector(".add-btn");
 const mainContent = document.querySelector(".main-content");
-const inputs = document.querySelectorAll('input');
 
-const myLibrary = [];
-
-function Book(author, title, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-}
-
-function addBookToLibrary() {
-  if (checkIsEmpty(authorInput) && checkIsEmpty(titleInput) && checkIsEmpty(pagesInput) && checkIsEmpty(readInput) && authorInput.checkValidity() === true && titleInput.checkValidity() === true && pagesInput.checkValidity() === true && readInput.checkValidity() === true) {
-    let book = new Book(authorInput.value, titleInput.value, pagesInput.value, readInput.value.toLowerCase());
-    let colors = randomColors();
-    let readColor = "#000000";
-    let notReadcolor = "#777777";
-
-    myLibrary.push(book);
-    
+class Library {
+  constructor(author, title, pages, read) {
+    let colors = Library.randomColors();
     let bookContainer = document.createElement("div")
     let bookSectionOne = document.createElement("div");
     let bookSectionTwo = document.createElement("div");
     let bookSectionThree = document.createElement("div");
     let readBtn = document.createElement("button");
     let deleteBtn = document.createElement("button");
-    let author = document.createElement("div");
-    let title = document.createElement("div");
-    let pages = document.createElement("div");
-    let read = document.createElement("div");
+    let authorDiv = document.createElement("div");
+    let titleDiv = document.createElement("div");
+    let pagesDiv = document.createElement("div");
+    let readDiv = document.createElement("div");
+
     bookContainer.setAttribute("class", "book-container");
     bookSectionOne.setAttribute("class", "book-section-one");
     bookSectionTwo.setAttribute("class", "book-section-two");
@@ -43,85 +29,98 @@ function addBookToLibrary() {
     bookSectionOne.style.backgroundColor = colors.notRead;
     bookSectionThree.style.backgroundColor = colors.notRead; 
 
-    if (book.read === "yes") bookRead();
+    if (read === "yes") bookRead();
     else bookNotRead();
 
     readBtn.addEventListener("click", () => {
-      if (book.read === "yes") book.read = "no", bookNotRead();
-      else book.read = "yes", bookRead();
+      if (read === "yes") read = "no", bookNotRead();
+      else read = "yes", bookRead();
     })
 
     deleteBtn.addEventListener("click", () => {
       let deleteOrCancel = confirm("Do you accept?");
-      if (deleteOrCancel) myLibrary.splice(myLibrary.indexOf(book), 1), mainContent.removeChild(bookContainer);
+      if (deleteOrCancel) {
+        mainContent.removeChild(bookContainer)
+      }
     });
 
-    author.setAttribute("class", "author");
-    title.setAttribute("class", "title");
-    pages.setAttribute("class", "pages");
-    read.setAttribute("class", "test");
-    author.textContent = book.author;
-    title.textContent = book.title;
-    pages.textContent = book.pages + " pages";
-    read.textContent = book.read;
+    authorDiv.setAttribute("class", "author");
+    titleDiv.setAttribute("class", "title");
+    pagesDiv.setAttribute("class", "pages");
+    readDiv.setAttribute("class", "test");
+    authorDiv.textContent = author;
+    titleDiv.textContent = title
+    pagesDiv.textContent = pages + " pages";
+    readDiv.textContent = read;
     bookSectionOne.appendChild(readBtn);
     bookSectionOne.appendChild(deleteBtn);
-    bookSectionTwo.appendChild(title);
-    bookSectionTwo.appendChild(author);
-    bookSectionTwo.appendChild(pages);
+    bookSectionTwo.appendChild(titleDiv);
+    bookSectionTwo.appendChild(authorDiv);
+    bookSectionTwo.appendChild(pagesDiv);
     bookContainer.appendChild(bookSectionOne);
     bookContainer.appendChild(bookSectionTwo);
     bookContainer.appendChild(bookSectionThree);
     mainContent.appendChild(bookContainer);
 
-    inputs.forEach(input =>  input.value = "");
-    
-    function randomColors() {
-      const randomNumber = Math.floor(Math.random() * 10)
-      const colorsArray = [
-        // rose
-        {read: "#fb7185", notRead: "#fda4af"}, 
-        // cyan
-        {read: "#22d3ee", notRead: "#67e8f9"},
-        // yellow
-        {read: "#facc15", notRead: "#fde047"},
-        // purple
-        {read: "#c084fc", notRead: "#d8b4fe"}, 
-        // green
-        {read: "#4ade80", notRead: "#86efac"},
-        // blue
-        {read: "#60a5fa", notRead: "#93c5fd"},
-        // fuchsia
-        {read: "#e879f9", notRead: "#f0abfc"},
-        // teal
-        {read: "#2dd4bf", notRead: "#5eead4"},
-        // orange
-        {read: "#fb923c", notRead: "#fdba74"},
-        // pink
-        {read: "#f472b6", notRead: "#f9a8d4"}
-      ];
-      const colorsPick = colorsArray[randomNumber];
-      return colorsPick;
-    }
+    inputs.forEach(input => input.value = "")
 
     function bookRead() {
       bookSectionOne.style.backgroundColor = colors.read;
       bookSectionThree.style.backgroundColor = colors.read;
       readBtn.style.backgroundImage = "url(./images/svg/bookmark-check.svg)";
-      bookSectionTwo.style.color = readColor;
+      bookSectionTwo.style.color = Library.readColor;
     }
-    
+  
     function bookNotRead() {
       bookSectionOne.style.backgroundColor = colors.notRead;
       bookSectionThree.style.backgroundColor = colors.notRead;
       readBtn.style.backgroundImage = "url(./images/svg/bookmark-uncheck.svg)";
-      bookSectionTwo.style.color = notReadcolor;
+      bookSectionTwo.style.color = Library.notReadcolor;
     }
   }
 
-  function checkIsEmpty(input) {
-    if (!(input.value.split("").filter(value => value != " ").length === 0)) return true
+  static readColor = "#000000";
+  static notReadcolor = "#777777";
+
+  static randomColors() {
+    const randomNumber = Math.floor(Math.random() * 10)
+    const colorsArray = [
+      // rose
+      {read: "#fb7185", notRead: "#fda4af"}, 
+      // cyan
+      {read: "#22d3ee", notRead: "#67e8f9"},
+      // yellow
+      {read: "#facc15", notRead: "#fde047"},
+      // purple
+      {read: "#c084fc", notRead: "#d8b4fe"}, 
+      // green
+      {read: "#4ade80", notRead: "#86efac"},
+      // blue
+      {read: "#60a5fa", notRead: "#93c5fd"},
+      // fuchsia
+      {read: "#e879f9", notRead: "#f0abfc"},
+      // teal
+      {read: "#2dd4bf", notRead: "#5eead4"},
+      // orange
+      {read: "#fb923c", notRead: "#fdba74"},
+      // pink
+      {read: "#f472b6", notRead: "#f9a8d4"}
+    ];
+    const colorsPick = colorsArray[randomNumber];
+    return colorsPick;
   }
 }
 
-addBtn.addEventListener("click", addBookToLibrary);
+addBtn.addEventListener("click", () => {
+  let valid = true;
+
+  for (input of inputs) {
+    if (!(input.value.split("").filter(value => value != " ").length !== 0 && input.checkValidity() === true)) {
+      valid = false;
+    }
+  }
+
+  if (valid) {
+    new Library(authorInput.value, titleInput.value, pagesInput.value, readInput.value);
+  }
+})
